b4a6c86bc3131f77b32208dcd2f214dbdd20f776
woodwj <willwood808@gmail.com>
2022-09-06 17:38:52 +0100

    1st Prediction - Penrose Normal Equations
    Isolated Opening and Closing prices and trained weights using penrose inverserse. same training and testing, inaccurate error reported

--- scraper.py
+++ scraper.py
@@ -4,14 +4,14 @@ import numpy as np
 M_data = yf.download("BP SHEL", start="2017-01-01", end="2017-01-30")
 
 def clean(d):
-    # The ones column to be added
-    col = np.ones(shape = d.T.shape, dtype=int)
     
-    X = d["Open"].T
+    X = d["Open"].to_numpy().T
     print(X)
 
-    Y = d["Close"].T
+    Y = d["Close"].to_numpy().T
     print(Y)
 
 X_t, Y = clean(M_data)
+
+
 
