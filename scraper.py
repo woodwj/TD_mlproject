dcfefd8ed51a7ae3a656c93fd2abf86757be5bf3
woodwj <willwood808@gmail.com>
2022-09-06 16:07:39 +0100

    Importing Data
    Data can be imported from yfinance

--- scraper.py
+++ scraper.py
@@ -1,4 +1,17 @@
 import yfinance as yf
+import numpy as np
 
+M_data = yf.download("BP SHEL", start="2017-01-01", end="2017-01-30")
 
-print("hello world")
+def clean(d):
+    # The ones column to be added
+    col = np.ones(shape = d.T.shape, dtype=int)
+    
+    X = d["Open"].T
+    print(X)
+
+    Y = d["Close"].T
+    print(Y)
+
+X_t, Y = clean(M_data)
+
