f0746e6617adf29290f95fe8a1db1e5126d951f9
Josh Shepherd <86619383+ItsJoshShepherd@users.noreply.github.com>
2022-09-13 15:16:52 +0100

    Portfolio Optimisation Code
    Portfolio Optimisation using 2 different methods, both focusing on achieving highest possible Sharpe Ratio.

--- josh_scraper.py
+++ josh_scraper.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 
+import numpy
+
 #data for BP and SHEL 
 data = yf.download("BP SHEL", start="2017-01-01", end="2017-01-30")
 print(data["Close"])
@@ -8,6 +10,6 @@ print(data["Close"])
 DeltaClosePs = data.diff(axis=0)
 print(DeltaClosePs["Close"],"\n")
 
-ActualClose = yf.download("BP SHEL",start="2017-01-30",end="2017-01-31")
-print("Close prices for 31st Jan were:\n",ActualClose["Close"])
+#ActualClose = yf.download("BP SHEL",start="2017-01-30",end="2017-01-31")
+#print("Close prices for 31st Jan were:\n",ActualClose["Close"])
 
